a76802aecee25db4c23b64a0a26f2b143aef9f12
arturogajardo <gajardo.arturo@gmail.com>
2026-04-14 13:11:50 -0400

    cambios

--- arriendos/urls.py
+++ arriendos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.catalogo, name='inicio'),
     path('catalogo/', views.catalogo, name='catalogo'),
     path('arrendar/<int:maquina_id>/', views.solicitar_arriendo, name='solicitar_arriendo'),
 ]
