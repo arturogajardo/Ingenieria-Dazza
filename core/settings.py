83c995222a06ab2e7e0b083abc3c77a2e5380c9f
arturogajardo <gajardo.arturo@gmail.com>
2026-04-14 12:49:32 -0400

    settings

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3p07hec&&oo*dz@2^3at9***9vd4#o7ubp*75o*qz4e0_n(383
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
